0ff61839aa9062703d253c9ab8bcff6ac2c1129c
SirDrect <froly3566@gmail.com>
2025-07-17 09:29:50 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# HELA-X-MD-1.0
-Simple wa bot in to the sri lanka 
+# WA-BOT
---
+++ settings.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+
+SESSION_ID: process.env.SESSION_ID === undefined ? 'WA-BOT=SMlE1C6Q#vgrvJQsC5t-F1uAQ9eiwVsvYL8xu6OWKa0R23gCDOVY' : process.env.SESSION_ID,
+PREFIX: process.env.PREFIX || '.',
+LANG: process.env.LANG || 'EN',
+SUDO: process.env.SUDO === undefined ? '94704227534' : process.env.SUDO,
+MODE: process.env.MODE === undefined ?"privete" : process.env.MODE,
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS
+};
